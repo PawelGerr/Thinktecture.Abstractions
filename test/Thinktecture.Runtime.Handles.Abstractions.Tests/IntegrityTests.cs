09b6b2719e3ec220c2cc81002590b9cce4abfc26
Pawel Gerr <pawel.gerr@thinktecture.com>
2018-03-08 17:31:05 +0100

    Added integrity tests for runtime.handles

---
+++ test/Thinktecture.Runtime.Handles.Abstractions.Tests/IntegrityTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using Xunit;
+
+namespace Thinktecture
+{
+	public class IntegrityTests : IntegrityTestsBase
+	{
+		public IntegrityTests()
+			: base("Runtime.Handles")
+		{
+			ExcludedTypes.Add(typeof(CriticalHandle));
+			ExcludedTypes.Add(typeof(WaitHandleExtensions));
+		}
+
+		[Fact]
+		public void Should_contain_all_types()
+		{
+			CheckTypes();
+		}
+
+		[Fact]
+		public void Should_contain_all_members()
+		{
+			CheckMembers();
+		}
+	}
+}
